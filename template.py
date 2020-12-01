1f164b9285de56e2cfda4f3ceba61bcbdbf1a7f6
Barnaby Gray <barnaby@pickle.me.uk>
2020-12-01 12:33:22 +0000

    Initial commit

---
+++ template.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+def part1(data):
+    return None
+
+def part2(data):
+    return None
+
+if __name__ == '__main__':
+    data = open('inputX.txt').read()
+    print(part1(data))
+    # print(part2(data))
