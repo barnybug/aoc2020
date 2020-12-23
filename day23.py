477f419bf690cbcbff2a2ca8233834bdd1944dfb
Barnaby Gray <barnaby@pickle.me.uk>
2020-12-23 07:58:10 +0000

    Day 23

---
+++ day23.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+def play(cups, times):
+    n = max(cups)
+
+    nodes = [None] * (n+1)
+    for a, b in zip(cups, cups[1:] + cups[:1]):
+        nodes[a] = b
+
+    current = cups[0]
+    for i in range(times):
+        a = nodes[current]
+        b = nodes[a]
+        c = nodes[b]
+        nodes[current] = nodes[c]
+        destination = current
+        while True:
+            destination -= 1
+            if destination < 1:
+                destination = n
+            if destination != a and destination != b and destination != c:
+                break
+        
+        nodes[c] = nodes[destination]
+        nodes[destination] = a
+        current = nodes[current]
+    
+    i = 1
+    while True:
+        i = nodes[i]
+        if i == 1:
+            break
+        yield i
+
+def part1(data):
+    cups = list(map(int, data))
+    cups = play(cups, 100)
+    return ''.join(map(str, cups))
+
+def part2(data):
+    cups = list(map(int, data))
+    for i in range(9, 1000000):
+        cups.append(i+1)
+    it = play(cups, 10000000)
+    return next(it) * next(it)
+
+if __name__ == '__main__':
+    data = '253149867'
+    print(part1(data))
+    print(part2(data))
--- test_cases.py
+++ test_cases.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 import day01, day02, day03, day04, day05, day06, day07, day08, day09, day10
 import day11, day12, day13, day14, day15, day16, day17, day18, day19, day20
-import day21, day22
+import day21, day22, day23
 
 def test_day01_part1():
     assert day01.part1([1721,979,366,299,675,1456]) == 514579
@@ -561,3 +561,9 @@ def test_day22_part1():
 
 def test_day22_part2():
     assert day22.part2(day22_example) == 291
+
+def test_day23_part1():
+    assert day23.part1('389125467') == '67384529'
+
+def test_day23_part2():
+    assert day23.part2('389125467') == 149245887792
